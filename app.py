7efa69dbb092e5f18badd45973ef977924b1604e
sepeca15 <sepeca15@gmail.com>
2022-08-10 03:16:27 +0000

    new route

--- app.py
+++ app.py
@@ -5,28 +5,35 @@ import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_flask():
-    return "Hello Flask"
+    return 'Hello Flask'
 
-@app.route("/inicio")
+@app.route('/inicio')
 def show_home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/<string:country>/<string:variety>/<float:aroma>/<float:afterteaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>")
+@app.route('/<string:country>/<string:variety>/<float:aroma>/<float:aftertaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>')
+def result(country, variety, aroma, aftertaste, acidity, body, balance, moisture):
+    cols = ['country_of_origin', 'variety', 'aroma','aftertaste','acidity','body','balance','moisture']
+    data = [country, variety, aroma,aftertaste,acidity,body,balance,moisture]
+    posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
+    loaded_model = pickle.load(open('coffee_model.pkl','rb'))
+    result = loaded_model.predict(posted)
+    test_result = result.tolist()[0]
+    if test_result == 'Yes':
+        return jsonify(message = 'Si es un cafe de primera'), 200
+    else:
+        return jsonify(message = 'No es un cafe de primera'), 200
+
+@app.route("/pepe/<string:country>/<string:variety>/<float:aroma>/<float:afterteaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>")
 def url_variables(country, variety, aroma, afterteaste, acidity, body, balance, moisture):
-
     cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
     data = [country, variety, aroma, afterteaste, acidity, body, balance, moisture]
-
     posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
-
     loaded_model = pickle.load(open("../models/coffee_model.pkl", "rb"))
-
     result = loaded_model.predict(posted)
-
     text_result =  result.tolist()[0]
-
     if text_result == "Yes":
         return jsonify(message="Es un cafe de primera"), 200
     else:
