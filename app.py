44046b5349d7fedf17a590cbd2bce4a1cd2b482b
sepeca15 <sepeca15@gmail.com>
2022-08-11 02:26:15 +0000

    delete route

--- app.py
+++ app.py
@@ -22,18 +22,5 @@ def result(country, variety, aroma, aftertaste, acidity, body, balance, moisture
     else:
         return jsonify(message = 'No es un cafe de primera'), 200
 
-@app.route("/pepe/<string:country>/<string:variety>/<float:aroma>/<float:afterteaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>")
-def url_variables(country, variety, aroma, afterteaste, acidity, body, balance, moisture):
-    cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
-    data = [country, variety, aroma, afterteaste, acidity, body, balance, moisture]
-    posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
-    loaded_model = pickle.load(open("../models/coffee_model.pkl", "rb"))
-    result = loaded_model.predict(posted)
-    text_result =  result.tolist()[0]
-    if text_result == "Yes":
-        return jsonify(message="Es un cafe de primera"), 200
-    else:
-        return jsonify(message="No es un cafe de primera"), 200
-
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
